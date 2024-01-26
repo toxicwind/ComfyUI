15dae8cc7e5b2b76bba6b3264fb299fe90b06f2f
toxicwind <toxicwind@gmail.com>
2024-01-26 14:36:13 -0700

    .

--- nodes.py
+++ nodes.py
@@ -1420,11 +1420,20 @@ class SaveImage:
             metadata = None
             if not args.disable_metadata:
                 metadata = PngInfo()
-                if prompt is not None:
+                def is_json_serializable(obj):
+                    try:
+                        json.dumps(obj)
+                        return True
+                    except TypeError:
+                        return False
+
+                if prompt is not None and is_json_serializable(prompt):
                     metadata.add_text("prompt", json.dumps(prompt))
+
                 if extra_pnginfo is not None:
                     for x in extra_pnginfo:
-                        metadata.add_text(x, json.dumps(extra_pnginfo[x]))
+                        if is_json_serializable(extra_pnginfo[x]):
+                            metadata.add_text(x, json.dumps(extra_pnginfo[x]))
 
             file = f"{filename}_{counter:05}_.png"
             img.save(os.path.join(full_output_folder, file), pnginfo=metadata, compress_level=self.compress_level)
